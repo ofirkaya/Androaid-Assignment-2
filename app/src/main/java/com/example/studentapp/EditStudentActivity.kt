421c1ee5b0c67ad7b1166f195064d07272ce59d1
ofir kaya <kayaofir@gmail.com>
2026-01-17 01:30:48 +0200

    created layout files

--- app/src/main/java/com/example/studentapp/EditStudentActivity.kt
+++ app/src/main/java/com/example/studentapp/EditStudentActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import missing.namespace.R
 
 class EditStudentActivity : AppCompatActivity() {
     private lateinit var studentId: String
